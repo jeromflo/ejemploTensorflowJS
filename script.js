14a57e2072ecbc08faa6abff7fc304cbf33fc210
jesus <jeromflo@gmail.com>
2020-06-26 14:45:05 +0200

    continuando el aprendizaje con tensorflow, comentado y anyadido nuevos metodos y codigo

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var tf = require("@tensorflow/tfjs");
+//import * as tf from "@tensorflow/tfjs-node";
 var fs = require("fs"); //sirve para leer archivos
 var returned = fs.readFileSync("ex1data1.txt", "utf8"); //le digo el archivo que tiene q leer y el formato q tiene
 var filas = returned.split('\n'); //separo el texto por filas
@@ -20,13 +21,25 @@ var tensorY = tf.tensor(y, [y.length, 1], "float32"); //creo un tensor para la v
 //en ambos casos le digo el shape, es decir el tamanyo de la matriz y el tipo que queremos
 var model = tf.sequential();
 //creamos nuestro modelo
-var layer = tf.layers.dense({ units: 1, inputShape: [1] });
-//creamos nueestra capa de neuronas, en este caso, le decimos que queremos una neurona con una de entrada
+var layer = tf.layers.dense({ units: 100, inputShape: [1], activation: "relu" }); //inputshape, hace referencia, al numero de atributos de los datos
+//creamos nueestra capa de neuronas, en este caso, le decimos que queremos 100 neuronas de primera capa oculta 
+model.add(layer);
+layer = tf.layers.dropout({ rate: 0.2 }); //es parecido a una capa densa, pero es una capa intermedia, la cual tiene un 20% de prob de no transmitir la informacion
+//haciendo que no se haga mayor overfitting
+model.add(layer);
+layer = tf.layers.dense({ units: 50, activation: "relu", inputShape: [100] }); //siguiente capa oculta con 50 neuronas
+model.add(layer);
+layer = tf.layers.dense({ units: 100, activation: "relu", inputShape: [50] }); //siguiente capa oculta con 100 neuronas
+model.add(layer);
+layer = tf.layers.dense({ units: 1, activation: "relu" }); //capa de salida con una neurona, ya que solo predecimos un valor
 model.add(layer);
 //anyadimos la capa al modelo
-model.compile({ loss: 'meanSquaredError', optimizer: 'sgd' });
-//compilamos con la funcion de error de error medio y el optimizador sgd
-model.fit(tensorX, tensorY);
+model.compile({ loss: 'meanSquaredError', optimizer: "adam", metrics: tf.metrics.meanAbsoluteError });
+//compilamos con la funcion de error de error medio y el optimizador sgd o adam
+model.summary(); //metodo que imprime una lista con las capas
+model.fit(tensorX, tensorY, { epochs: 5 }); //epochs es el numero de iteracciones de backpropagetion
 //entrenamos el modelo
+//let evaluate=model.evaluate(tensorX,tensorY);
+//evaluate=tf.tensor(evaluate.);
 console.log(model.predict(tf.tensor2d([[5], [1]], [2, 1])).toString());
 //imprimimos la prediccion segun los valores pasados
--- script.ts
+++ script.ts
@@ -1,4 +1,5 @@
 import * as tf from "@tensorflow/tfjs";
+//import * as tf from "@tensorflow/tfjs-node";
 import * as fs from "fs";//sirve para leer archivos
 let returned = fs.readFileSync("ex1data1.txt", "utf8");//le digo el archivo que tiene q leer y el formato q tiene
 
@@ -25,16 +26,29 @@ let tensorY=tf.tensor(y,[y.length,1],"float32");//creo un tensor para la variabl
 let model=tf.sequential();
 //creamos nuestro modelo
 
-let layer=tf.layers.dense({units:1,inputShape:[1]});
-//creamos nueestra capa de neuronas, en este caso, le decimos que queremos una neurona con una de entrada
+let layer=tf.layers.dense({units:100,inputShape:[1],activation:"relu"});//inputshape, hace referencia, al numero de atributos de los datos
+//creamos nueestra capa de neuronas, en este caso, le decimos que queremos 100 neuronas de primera capa oculta 
 model.add(layer);
+ layer=tf.layers.dropout({rate:0.2});//es parecido a una capa densa, pero es una capa intermedia, la cual tiene un 20% de prob de no transmitir la informacion
+ //haciendo que no se haga mayor overfitting
+ model.add(layer);
+ layer=tf.layers.dense({units:50,activation:"relu",inputShape:[100]});//siguiente capa oculta con 50 neuronas
+ model.add(layer);
+ layer=tf.layers.dense({units:100,activation:"relu",inputShape:[50]});//siguiente capa oculta con 100 neuronas
+ model.add(layer);
+ layer=tf.layers.dense({units:1,activation:"relu"});//capa de salida con una neurona, ya que solo predecimos un valor
+ model.add(layer);
+
+
 //anyadimos la capa al modelo
-model.compile({ loss: 'meanSquaredError', optimizer: 'sgd' });
-//compilamos con la funcion de error de error medio y el optimizador sgd
+model.compile({ loss: 'meanSquaredError', optimizer:"adam" ,metrics:tf.metrics.meanAbsoluteError});
+//compilamos con la funcion de error de error medio y el optimizador sgd o adam
+model.summary();//metodo que imprime una lista con las capas
 
-model.fit(tensorX,tensorY);
+model.fit(tensorX,tensorY,{epochs:5});//epochs es el numero de iteracciones de backpropagetion
+
 //entrenamos el modelo
-
-
-console.log(model.predict(tf.tensor2d([[5],[1]], [2, 1])).toString());
+//let evaluate=model.evaluate(tensorX,tensorY);
+    //evaluate=tf.tensor(evaluate.);
+ console.log(model.predict(tf.tensor2d([[5],[1]], [2, 1])).toString());
 //imprimimos la prediccion segun los valores pasados
