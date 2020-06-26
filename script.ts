97d5d8a47e8940e2de1765ea34b40341fb28f0a1
jesus <jeromflo@gmail.com>
2020-06-26 12:24:45 +0200

    anyadidos los comentarios al codigo de ejemplo y modificado algunas cosas sobrantes

--- script.ts
+++ script.ts
@@ -1,13 +1,12 @@
 import * as tf from "@tensorflow/tfjs";
-import * as fs from "fs";
-import { tensor } from "@tensorflow/tfjs";
-let returned = fs.readFileSync("ex1data1.txt", "utf8");
+import * as fs from "fs";//sirve para leer archivos
+let returned = fs.readFileSync("ex1data1.txt", "utf8");//le digo el archivo que tiene q leer y el formato q tiene
 
 
-let filas = returned.split('\n');
+let filas = returned.split('\n');//separo el texto por filas
 let array: any[][] = [[], []];
 for (let i = 0; i < filas.length - 1; i++) {
-    let columnas: any[] = filas[i].split(',');
+    let columnas: any[] = filas[i].split(',');//separo los datos en columnas segun las ","
 
     array[i] = columnas;
     //console.log(columnas[0]);
@@ -19,7 +18,7 @@ let y: number[]=[];
 array=array as number[][];
 //console.log(typeof(array[0][0]));
 for (let i = 0; i < array.length; i++) {
-        x[i]=<number>array[i][0];
+        x[i]=array[i][0];
         y[i]=array[i][1];
 }
 //console.log(x);
@@ -38,10 +37,3 @@ model.fit(tensorX,tensorY);
 
 
 console.log(model.predict(tf.tensor2d([[5],[1]], [2, 1])).toString());
-/*model.add(tf.layers.dense({ units: 1 }));
-
-
-model.compile({ loss: 'meanSquaredError', optimizer: 'sgd' });
-//model.fit(tensorX,tensorY);
-console.log(model.predict(tf.tensor2d([5], [1, 1])).toString());
-tensorX.print();*/
