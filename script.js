69f4ba35c21e3848baeaafc84d50e38c1c110353
jesus <jeromflo@gmail.com>
2020-06-26 12:25:17 +0200

    anyadidos los comentarios al codigo de ejemplo y modificado algunas cosas sobrantes

--- script.js
+++ script.js
@@ -8,22 +8,25 @@ var array = [[], []];
 for (var i = 0; i < filas.length - 1; i++) {
     var columnas = filas[i].split(','); //separo los datos en columnas segun las ","
     array[i] = columnas;
-    //console.log(columnas[0]);
 }
 var x = [];
 var y = [];
-array = array;
-//console.log(typeof(array[0][0]));
 for (var i = 0; i < array.length; i++) {
     x[i] = array[i][0];
     y[i] = array[i][1];
 }
-//console.log(x);
-var tensorX = tf.tensor(x, [x.length, 1], "float32");
-var tensorY = tf.tensor(y, [y.length, 1], "float32");
+var tensorX = tf.tensor(x, [x.length, 1], "float32"); //creo el tensor para la variable x
+var tensorY = tf.tensor(y, [y.length, 1], "float32"); //creo un tensor para la variable y
+//en ambos casos le digo el shape, es decir el tamanyo de la matriz y el tipo que queremos
 var model = tf.sequential();
+//creamos nuestro modelo
 var layer = tf.layers.dense({ units: 1, inputShape: [1] });
+//creamos nueestra capa de neuronas, en este caso, le decimos que queremos una neurona con una de entrada
 model.add(layer);
+//anyadimos la capa al modelo
 model.compile({ loss: 'meanSquaredError', optimizer: 'sgd' });
+//compilamos con la funcion de error de error medio y el optimizador sgd
 model.fit(tensorX, tensorY);
+//entrenamos el modelo
 console.log(model.predict(tf.tensor2d([[5], [1]], [2, 1])).toString());
+//imprimimos la prediccion segun los valores pasados
--- script.ts
+++ script.ts
@@ -9,31 +9,32 @@ for (let i = 0; i < filas.length - 1; i++) {
     let columnas: any[] = filas[i].split(',');//separo los datos en columnas segun las ","
 
     array[i] = columnas;
-    //console.log(columnas[0]);
 
 }
 
 let x: number[]=[];
 let y: number[]=[];
-array=array as number[][];
-//console.log(typeof(array[0][0]));
 for (let i = 0; i < array.length; i++) {
         x[i]=array[i][0];
         y[i]=array[i][1];
 }
-//console.log(x);
 
-let tensorX=tf.tensor(x,[x.length,1],"float32");
-let tensorY=tf.tensor(y,[y.length,1],"float32");
+let tensorX=tf.tensor(x,[x.length,1],"float32");//creo el tensor para la variable x
+let tensorY=tf.tensor(y,[y.length,1],"float32");//creo un tensor para la variable y
+//en ambos casos le digo el shape, es decir el tamanyo de la matriz y el tipo que queremos
 let model=tf.sequential();
+//creamos nuestro modelo
 
 let layer=tf.layers.dense({units:1,inputShape:[1]});
-
+//creamos nueestra capa de neuronas, en este caso, le decimos que queremos una neurona con una de entrada
 model.add(layer);
+//anyadimos la capa al modelo
 model.compile({ loss: 'meanSquaredError', optimizer: 'sgd' });
-
+//compilamos con la funcion de error de error medio y el optimizador sgd
 
 model.fit(tensorX,tensorY);
+//entrenamos el modelo
 
 
 console.log(model.predict(tf.tensor2d([[5],[1]], [2, 1])).toString());
+//imprimimos la prediccion segun los valores pasados
